c681b2d16adfcb0979c93faaeb03f55ce95a8cb7
wanghuan <21631068@zju.edu.cn>
2018-01-26 16:00:31 +0800

    add

--- Python/log.py
+++ Python/log.py
@@ -15,8 +15,17 @@ import sys
 assert (len(sys.argv) == 3)
 
 if sys.argv[2] in ('step', 'Step'):
-    assert('_prune.txt' in sys.argv[1])
-    os.system('cat %s | grep "Step" > %s' % (sys.argv[1], sys.argv[1].replace('_prune.txt', '_Step.txt')))
+    if os.path.isdir(sys.argv[1]):
+        prune_txts = [os.path.join(sys.argv[1], i) for i in os.listdir(sys.argv[1]) if i.endswith("prune.txt")]
+        if len(prune_txts) != 1:
+            print("There are not 1 `prune.txt` in your provided directory, please check.")
+            print (prune_txts)
+            exit(1)
+        inFile = prune_txts[0]
+    else:
+        inFile = sys.argv[1]
+        assert(inFile.endswith("prune.txt"))
+    os.system('cat %s | grep "Step" > %s' % (inFile, inFile.replace('_prune.txt', '_Step.txt')))
     
 elif sys.argv[2] in ('acc'):
     os.system('cat %s | grep "accuracy = "' % sys.argv[1])
